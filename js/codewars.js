d7a9ca005ef352e4cd6764355ceacca5a3e0d9cd
Austin Varnon <austinvarnon@gmail.com>
2020-08-05 13:19:55 -0500

    almost done with sales tax exercise

--- js/codewars.js
+++ js/codewars.js
@@ -16,9 +16,9 @@
 //     return userInput;
 // }
 //----------------Correct Way--------------------
-function disemvowel(str) {
-    return str.replace(/[aeiou]/gi, '');
-}
+// function disemvowel(str) {
+//     return str.replace(/[aeiou]/gi, '');
+// }
 
 // g: tells to find all matches, not just the first.
 //
@@ -34,35 +34,58 @@ function disemvowel(str) {
 //The array is either entirely comprised of odd integers or entirely comprised of even integers except
 //for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
 
-function findOutlier(arr){
-   let odds = arr.filter(function (number) {
-       return !(number % 2);
-   });
-   let evens = arr.filter(function (number) {
-       return !!(number % 2);
-   });
-
-   let result = (odds.length < evens.length) ? odds.pop() : evens.pop();
-   return result;
-}
+// function findOutlier(arr){
+//    let odds = arr.filter(function (number) {
+//        return !(number % 2);
+//    });
+//    let evens = arr.filter(function (number) {
+//        return !!(number % 2);
+//    });
+//
+//    let result = (odds.length < evens.length) ? odds.pop() : evens.pop();
+//    return result;
+// }
 
 
 //--------------------------------------------------------------------------------
-//sales tax exercise
+//sales tax calculator exercise
 
 let shoppingCart = [];
-
-function totalTax(input) {
-    return (input + (input * .15));
+function basicTax(input) {
+    return (input * .10);
 }
-
-function exemptOfTax(input) {
+//--------
+function importTax(input) {
     // exempt for food, books, medical. 10% tax
-    return (input + (input * .05));
+    return (input * .05);
+}
+//--------
+function createItem(name, price, type, quantity, shoppingCart) {
+    let newItem = {
+        name: name,
+        price: price,
+        type: type,
+        quantity: quantity
+    }
+    shoppingCart.push(newItem);
+    return shoppingCart;
+}
+createItem("ToothBrush", 3.00, "hygeine", 1, shoppingCart);
+//--------
+function printReceipt(shoppingCart) {
+    // let exemptItemTypes = ["book", "food", "medical"];
+    shoppingCart.forEach(function (item) {
+        console.log(item.quantity + "" + item.name + ": $" + ((item.price).toFixed(2) * item.quantity));
+        if(item.type === "book" || item.type ===  "food"|| item.type ===  "medical"){
+            return item.quantity + " " + item.name + ": $" + importTax(item.price).toFixed(2) + basicTax(0);
+        }
+        console.log("Sales Taxes: $" + importTax(item.price).toFixed(2) + basicTax(item.price).toFixed(2));
+        console.log("Total price is: $" + (item.price) + importTax(item.price).toFixed(2) + basicTax(item.price).toFixed(2));
+    })
 }
 
-
-
+// createItem("x", "y",...shoppingCart)
+// printReceipt(shoppingCart);
 
 
 
