cc00e9a2de44552c4aa7d6a0c40e01ccec176e05
Austin Varnon <austinvarnon@gmail.com>
2020-08-19 09:44:50 -0500

    Second exercise done

--- js/codewars8-19.js
+++ js/codewars8-19.js
@@ -8,10 +8,29 @@ var isSquare = function(n){
     }
     return false;
 }
+//NOTE:   isInteger return a boolean answer.
+
+// function isSquare(n) {
+//     return Math.sqrt(n) % 1 === 0;
+// }
+
+// const isSquare = n => Number.isInteger(Math.sqrt(n));
 
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Validating PIN code~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+function validatePIN (pin) {
+    var pinlen = pin.length;
+    var isCorrectLength = (pinlen === 4 || pinlen === 6);
+    var hasOnlyNumbers = pin.match(/^\d+$/);
 
+    if(isCorrectLength && hasOnlyNumbers){
+        return true;
+    }
+    return false;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Validating PIN code~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
